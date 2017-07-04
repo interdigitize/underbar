fce2712665b2c29f984c5995476d639fe1401742
interdigitize <interdigitize@users.noreply.github.com>
2017-07-04 15:15:04 -0700

    Slight refactor to reject.

--- src/underbar.js
+++ src/underbar.js
@@ -100,7 +100,7 @@
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
     var rejected = _.filter(collection, function(el){
-      if(!test(el) !== true){
+      if(!test(el)){
         return true;
       }
     });

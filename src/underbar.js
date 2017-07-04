d2bc8213d4e98520dd3e189f62ab5f7ca80ed925
interdigitize <interdigitize@users.noreply.github.com>
2017-07-04 14:41:19 -0700

    Completes last.

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    }
+    if (n > array.length) {
+      return array;
+    }
+    return n === undefined ? array[array.length - 1] : array.slice(array.length - n, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
